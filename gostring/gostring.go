10469fa2e5bf47ae01cfdc2b69f4e5ab4f18b4ca
Arup3201 <arupjana7365@gmail.com>
2025-08-19 12:06:08 +0530

    first commit with sample go library functions

---
+++ gostring/gostring.go
@@ -0,0 +1,17 @@
+package gostring
+
+func CapitalizeWords(s string) string {
+	return ""
+}
+
+func CountVowelsConsonents(s string) (int, int) {
+	return 0, 0
+}
+
+func IsPalindrome(s string) bool {
+	return false
+}
+
+func ReverseString(s string) string {
+	return ""
+}
