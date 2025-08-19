ce32841ab7b8ca154fa40473555b052c6a3ed34e
Arup3201 <arupjana7365@gmail.com>
2025-08-19 12:20:13 +0530

    update: go module init and usage in main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Arup3201/gostring
+
+go 1.24.5
--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/Arup3201/gostring/gostring"
+)
+
+func main() {
+	bookTitle := "the lord of the rings"
+
+	fmt.Printf("Original: %s\n", bookTitle)
+	fmt.Printf("Capitalize: %s\n", gostring.CapitalizeWords(bookTitle))
+
+	vowels, consonents := gostring.CountVowelsConsonents(bookTitle)
+	fmt.Printf("Vowels: %d, Consonents: %d\n", vowels, consonents)
+	fmt.Printf("Is palindrome? %t\n", gostring.IsPalindrome(bookTitle))
+	fmt.Printf("Reversed: %s\n", gostring.ReverseString(bookTitle))
+}
