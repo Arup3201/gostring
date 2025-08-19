4a54aa8e20efbf384aecdebc59c7ccbbb1c414e7
Arup3201 <arupjana7365@gmail.com>
2025-08-19 12:33:26 +0530

    feat: capitalize string

--- gostring/gostring.go
+++ gostring/gostring.go
@@ -1,7 +1,20 @@
 package gostring
 
+import (
+	"strings"
+)
+
 func CapitalizeWords(s string) string {
-	return ""
+	var firstLetter string
+	words := strings.Fields(s)
+	cap := []string{}
+
+	for _, w := range words {
+		firstLetter = strings.ToUpper(string(w[0]))
+		cap = append(cap, firstLetter+w[1:])
+	}
+
+	return strings.Join(cap, " ")
 }
 
 func CountVowelsConsonents(s string) (int, int) {
